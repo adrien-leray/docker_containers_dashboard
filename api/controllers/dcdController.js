5f0f78575b781a3584955a2d486db3290fc94e8d
Adrien Leray <adrienleray131295@gmail.com>
2018-10-03 21:52:22 +0200

    improve view + add get by container id

--- api/controllers/dcdController.js
+++ api/controllers/dcdController.js
@@ -19,6 +19,40 @@ exports.list_all_containers = function(req, res) {
 };
 
 /**
+ * Get container by id
+ */
+exports.list_container_by_id = function(req, res) {
+  let container = docker.getContainer(req.params.id);
+
+  container.inspect((err, data) => {
+    res.render("container", {
+      container: data,
+      createdOn: new Date(data.Created).toDateString(),
+      createdTime: new Date(data.Created).toLocaleTimeString(),
+      startedOn: new Date(data.State.StartedAt).toDateString(),
+      startedTime: new Date(data.State.StartedAt).toLocaleTimeString(),
+      finishedOn: new Date(data.State.FinishedAt).toDateString(),
+      finishedTime: new Date(data.State.FinishedAt).toLocaleTimeString()
+    });
+  });
+};
+
+/**
+ * API
+ */
+
+/**
+ * Get container by id return json
+ */
+exports.list_container_by_id_json = function(req, res) {
+  let container = docker.getContainer(req.params.id);
+
+  container.inspect((err, data) => {
+    res.json(data);
+  });
+};
+
+/**
  * Get all json containers method
  * @param {*} req
  * @param {*} res
--- api/routes/dcdRoutes.js
+++ api/routes/dcdRoutes.js
@@ -8,10 +8,10 @@ module.exports = function(app) {
     .route("/containers")
     .get(dcd.list_all_containers)
     .post(dcd.create_container);
+  app.route("/containers/:id").get(dcd.list_container_by_id);
 
   // REST API
-
   app.route("/api/images").get(dcd.list_all_images);
-
   app.route("/api/containers").get(dcd.list_all_containers_json);
+  app.route("/api/containers/:id").get(dcd.list_container_by_id_json);
 };
